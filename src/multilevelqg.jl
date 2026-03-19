71944378e4f1684648ca8c1eb729132eb381925b
mpudig <mattpudig1998@gmail.com>
2026-03-18 20:22:04 -0400

    Fix minus sign typo. Set buoyancy to zero temporarily.

--- src/multilevelqg.jl
+++ src/multilevelqg.jl
@@ -270,11 +270,11 @@ U_invadv = copy(U) # inverse of advecting version of U, where U_0 = 2U_1/2 - U_1
 @views @. U_invadv[:, :, 1] = 2 * U[:, :, 1] - U[:, :, 2]
 @views @. U_invadv[:, :, end] = 2 * U[:, :, end] - U[:, :, end - 1]
 
-@views @. Qy[:, :, 1] -= Fup * (U_invadv[:, :, 1] - U_invadv[:, :, 2])
+@views @. Qy[:, :, 1] -= 0 #Fup * (U_invadv[:, :, 1] - U_invadv[:, :, 2])
 for j = 2 : nlevels + 1
-    @views @. Qy[:, :, j] -= Fm[j - 1] * (U_invadv[:, :, j] - U_invadv[:, :, j - 1]) + Fp[j - 1] * (U_invadv[:, :, j] - U_invadv[:, :, j + 1])
+    @views @. Qy[:, :, j] += Fm[j - 1] * (U_invadv[:, :, j] - U_invadv[:, :, j - 1]) + Fp[j - 1] * (U_invadv[:, :, j] - U_invadv[:, :, j + 1])
 end
-@views @. Qy[:, :, end] -= Flo * (U_invadv[:, :, end - 1] - U_invadv[:, :, end])
+@views @. Qy[:, :, end] -= 0 #Flo * (U_invadv[:, :, end - 1] - U_invadv[:, :, end])
 
   return Params(nlevels, T(f₀), T(β), Tuple(T.(N²)), T.(H), U, eta, topographic_gradient, T(r), T(ν), nν, calcFq, Qx, Qy, S, S⁻¹, rfftplanlayered)
 end
