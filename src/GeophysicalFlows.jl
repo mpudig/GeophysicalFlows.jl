9d91e423614f9d820b2d9206850447ea47a7f564
mpudig <mattpudig1998@gmail.com>
2026-07-06 10:59:59 -0400

    Forgot to export new module name.

--- src/GeophysicalFlows.jl
+++ src/GeophysicalFlows.jl
@@ -18,14 +18,14 @@ include("utils.jl")
 include("twodnavierstokes.jl")
 include("singlelayerqg.jl")
 include("multilayerqg.jl")
-include("multilevelqg.jl")
+include("qg3d.jl")
 include("surfaceqg.jl")
 include("barotropicqgql.jl")
 
 @reexport using GeophysicalFlows.TwoDNavierStokes
 @reexport using GeophysicalFlows.SingleLayerQG
 @reexport using GeophysicalFlows.MultiLayerQG
-@reexport using GeophysicalFlows.MultiLevelQG
+@reexport using GeophysicalFlows.QG3D
 @reexport using GeophysicalFlows.SurfaceQG
 @reexport using GeophysicalFlows.BarotropicQGQL
 
